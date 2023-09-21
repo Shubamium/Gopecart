c068cfc5feb77e6c8f1eb913b1da62d9bba139cb
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-09-22 01:27:53 +0700

    Fix book z-index issue

--- src/app/exhibition/book/page.tsx
+++ src/app/exhibition/book/page.tsx
@@ -1,14 +1,30 @@
 'use client'
-import { useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 import './book.scss'
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 export default function BooksPage() {
 	const [bookIndex,setBookIndex] = useState(0)
-	const bookData = ['hey','heeey','heoo'];
-	const prevPage = ()=>{setBookIndex((prev)=> Math.max(prev - 1,0))};
-	const nextPage = ()=>{setBookIndex((prev)=> Math.min(prev + 1,bookData.length ))};
-	
+	const [delay,setDelay] = useState(false);
+
+	const bookData = ['hey','heeey','heoo','heeye','egegeg','awdawdaw'];
+	const delayTime = 800;
+	const prevPage = ()=>{
+		setDelay(true)
+		if(delay)return;
+		
+		setBookIndex((prev)=> Math.max(prev - 1,0))
+
+		setTimeout(()=>{setDelay(false)},delayTime);
+	};
+	const nextPage = ()=>{
+		setDelay(true)
+		if(delay)return;
+
+		setBookIndex((prev)=> Math.min(prev + 1,bookData.length))
+
+		setTimeout(()=>{setDelay(false)},delayTime);
+	};
 	const stickers = [
 		'https://i.ibb.co/QrccgmM/sticker-1.png',
 		'https://i.ibb.co/yWFMnrc/sticker-2.png',
@@ -20,12 +36,23 @@ export default function BooksPage() {
 			<div className="book">
 				{bookData.map((data,index)=>{
 					const isFlipped = bookIndex > index
+					const range = 2
+					const isNear = index <= bookIndex+range && index >= bookIndex-range; 
+					const isNearBack = index < bookIndex+range-1 && index > bookIndex-range-1; 
+					const isActive = bookIndex === index
+
+					const setZIndex = ()=>{
+						return isFlipped ? 100+index : bookData.length-index-1;
+					}
 					return ( 
-						<div className={`page ${isFlipped ? 'flipped' : ''}`} key={`page-no-${index}`} style={{zIndex:isFlipped ? index : -index}} >
-							<div className="page-side front" onClick={nextPage} onDoubleClick={()=>{console.log(index,-index)}}>
+						<div 
+							className={`page ${isFlipped ? 'flipped' : ''} ${isNear ? 'near' : ''} ${isNear ? ' visible-img':' hidden-img'}`} 
+							key={`page-no-${index}`} 
+							style={{zIndex: isFlipped ? (bookIndex === index ? 200 : index+1) : -index+1}}>
+							<div className="page-side front" onClick={nextPage}>
 								<section className="sticker-showcase top">
 									<div className="img-part">
-										<img src={stickers[0]} alt="" />
+										{isNear && <img src={stickers[0]} alt="" />}
 									</div>
 									<div className="text-part">
 										<h2>Post-it Note</h2>
@@ -35,22 +62,22 @@ export default function BooksPage() {
 								</section>
 								<section className="sticker-showcase bottom">
 									<div className="text-part">
-										<h2>Post-it Note</h2>
+										<h2>Post-it Note {index}</h2>
 										<hr />
 										<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, atque. Asperiores inventore aliquid commodi voluptatum ipsam enim explicabo minus, in a nihil natus deserunt provident fugit atque libero quidem. Est facilis ullam quisquam accusantium tempora sunt architecto aut odio dolorem aliquam, totam ipsa illo reiciendis id. Aperiam quis ab numquam?</p>
 									</div>
 									<div className="img-part">
-										<img src={stickers[1]} alt="" />
+										{isNear && <img src={stickers[1]} alt="" />}
 									</div>
 								</section>
 							</div>
 							<div className="page-side back" onClick={prevPage}>
 							<section className="sticker-showcase top">
 									<div className="img-part">
-										<img src={stickers[3]} alt="" />
+										{isNearBack && <img src={stickers[3]} alt="" />}
 									</div>
 									<div className="text-part">
-										<h2>Post-it Note</h2>
+										<h2>Post-it Note{index} back</h2>
 										<hr />
 										<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, atque. Asperiores inventore aliquid commodi voluptatum ipsam enim explicabo minus, in a nihil natus deserunt provident fugit atque libero quidem. Est facilis ullam quisquam accusantium tempora sunt architecto aut odio dolorem aliquam, totam ipsa illo reiciendis id. Aperiam quis ab numquam?</p>
 									</div>
@@ -62,7 +89,7 @@ export default function BooksPage() {
 										<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, atque. Asperiores inventore aliquid commodi voluptatum ipsam enim explicabo minus, in a nihil natus deserunt provident fugit atque libero quidem. Est facilis ullam quisquam accusantium tempora sunt architecto aut odio dolorem aliquam, totam ipsa illo reiciendis id. Aperiam quis ab numquam?</p>
 									</div>
 									<div className="img-part">
-										<img src={stickers[2]} alt="" />
+										{isNearBack && <img src={stickers[2]} alt="" />}
 									</div>
 								</section>
 							</div>
