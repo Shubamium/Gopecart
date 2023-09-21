db1fbbacd286402c411e18ab4fbd9a9c7b7441ac
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-09-22 04:00:20 +0700

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,30 @@ import Footer from '@/components/general/footer/Footer'
 const amarante = Amarante({ subsets: ['latin'],weight:'400',variable:"--font-main" })
 const baskerville = Baskervville({ subsets: ['latin'], weight:'400',variable:"--font-secondary" })
 
+const title = 'Gopec Art Shop c:';
+const description = " Hey Guys, I'm Gopec D. Art! An awesome artist who loves to create. A jack of all trades from doodles to masterpieces, let me help make your ideas a digital reality! I hope you enjoy your visit c:";
+const banner = "https://i.ibb.co/G0MsZqD/embedlinkicon.png"
+const url = 'https://gopecartshop.vercel.app'
 export const metadata: Metadata = {
-  title: 'Gopecart',
-  description: 'Gopecart site',
+  title: title,
+  description: description,
+  metadataBase: new URL(url),
+  openGraph:{
+	  url:url,
+	  title:title,
+	  description:description,
+	  authors:"shubamium",
+	  images:[
+		  banner
+	  ]
+  },
+	  twitter:{
+	  title:title,
+	  card:'summary_large_image',
+	  images:[
+		  banner
+	  ]
+  },
 }
 
 export default function RootLayout({
